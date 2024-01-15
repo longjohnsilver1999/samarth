ed2df7609e433ca4392809fc1a3d4972e06863f4
sumit katyal <skatyal1999@gmail.com>
2024-01-15 11:24:23 +0530

    ui started

--- frontendzfs/src/App.js
+++ frontendzfs/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Main from './components/Main.jsx'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Inventory</h1>
+    <div>
+      <h3>Select SUPPLIER</h3>
+      <select onChange={}>
+        <option value="0" selected>Select</option>
+        <option value="">Dummy </option>
+      </select>
+    </div>
+    <div>
+      <h3>Select Item</h3>
+      <select onChange={}>
+        <option value="0" selected>Select</option>
+        <option value="">Dummy </option>
+      </select>
+    </div>
+    <div>
+      <h3>Select Quantity</h3>
+      <select onChange={}>
+        <option value="0" selected>Select</option>
+        <option value="">Dummy </option>
+      </select>
+    </div>
     </div>
   );
 }
---
+++ frontendzfs/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {useState} from "react";
+function Main() {
+    return (
+      <div >
+    <form action=""></form>
+      </div>
+    );
+  }
+  
+  export default Main;
