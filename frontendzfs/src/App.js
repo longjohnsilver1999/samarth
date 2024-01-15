70321fdcff2bb5bba83b668254fd12f8fb23699b
sumit katyal <skatyal1999@gmail.com>
2024-01-15 16:12:04 +0530

    ui start

--- frontendzfs/src/App.js
+++ frontendzfs/src/App.js
@@ -4,28 +4,8 @@ import Main from './components/Main.jsx'
 function App() {
   return (
     <div className="App">
-    <h1>Inventory</h1>
-    <div>
-      <h3>Select SUPPLIER</h3>
-      <select onChange={}>
-        <option value="0" selected>Select</option>
-        <option value="">Dummy </option>
-      </select>
-    </div>
-    <div>
-      <h3>Select Item</h3>
-      <select onChange={}>
-        <option value="0" selected>Select</option>
-        <option value="">Dummy </option>
-      </select>
-    </div>
-    <div>
-      <h3>Select Quantity</h3>
-      <select onChange={}>
-        <option value="0" selected>Select</option>
-        <option value="">Dummy </option>
-      </select>
-    </div>
+  < Main />
+  
     </div>
   );
 }
--- frontendzfs/src/components/Main.jsx
+++ frontendzfs/src/components/Main.jsx
@@ -1,9 +1,38 @@
 import React from "react";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 function Main() {
+  const [items,setitems]=useState([])
+  const [supplier,setsupplier]=useState([])
+  const [quantity,setquantity]=useState(0)
+const fetchSuppliers=async()=>{
+const displaysub=await fetch()
+}
+
     return (
       <div >
-    <form action=""></form>
+    <form action="">
+    <h1>Inventory</h1>
+    <div>
+      <h3>Select SUPPLIER</h3>
+      <select onChange={}>
+        <option value="0" selected>Select</option>
+        <option value="">Dummy </option>
+      </select>
+    </div>
+    <div>
+      <h3>Select Item</h3>
+      <select onChange={}>
+        <option value="0" selected>Select</option>
+        <option value="">Dummy </option>
+      </select>
+    </div>
+    <div>
+      <h3>Select Quantity</h3>
+      <select onChange={}>
+    <input type="number" />
+      </select>
+    </div>
+    </form>
       </div>
     );
   }
